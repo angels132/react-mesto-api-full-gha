ea2663752c4b8e0a5a2cdf7f9442f2f0c9a3409f
Ilya <ravilov.ilia@yandex.ru>
2023-09-07 20:34:09 +0700

    Проект 15

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, SECRET_SIGNING_KEY } = require('../utils/constants');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = (req, _, next) => {
+  const { authorization } = req.headers;
+  const bearer = 'Bearer ';
+
+  if (!authorization || !authorization.startsWith(bearer)) {
+    return next(new UnauthorizedError('Неправильные почта или пароль'));
+  }
+
+  const token = authorization.replace(bearer, '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? SECRET_SIGNING_KEY : 'dev-secret');
+  } catch (err) {
+    return next(new UnauthorizedError('Неправильные почта или пароль'));
+  }
+
+  req.user = payload;
+
+  return next();
+};
