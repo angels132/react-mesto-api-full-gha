c7bab4d18ba86d873e469c04c9d43367f05390dc
Ilya <ravilov.ilia@yandex.ru>
2023-09-11 21:38:36 +0700

    Проект 15.6 исправления подключение по api 2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -90,7 +90,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "https://api.mikuname.students.nomoredomainsicu.ru/",
+  baseUrl: "https://api.mikuname.students.nomoredomainsicu.ru",
   headers: {
     authorization: "43bc2f0f-65e6-40f5-a20b-329dc405ffe8",
     "Content-Type": "application/json",
